75a20fbcdf01b721ed5478d67885856e376a9906
Maria108 <maria.abash@gmail.com>
2021-03-25 09:58:14 -0400

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <p>Please choose your movie.</p>
     <p>Now go build something great.</p>
     <StaticImage
       src="../images/gatsby-astronaut.png"
